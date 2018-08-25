a5e4cf29496ab921bc24f10670521f042b0e6ea9
Gabarros <gabarros17@gmail.com>
2018-08-24 21:17:24 -0300

    Updata MainActivity

--- app/src/main/java/br/edu/iff/pooa20181/ondeir/activity/Principal.java
+++ app/src/main/java/br/edu/iff/pooa20181/ondeir/activity/Principal.java
@@ -6,14 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import br.edu.iff.pooa20181.ondeir.R;
 
-public class Principal extends AppCompatActivity implements AdapterView.OnItemClickListener{
+public class Principal extends AppCompatActivity{
 
     private String[] activities = {"ListaEvento"};
     private String[] itemMenu = {"Adicionar Tarefas do Dia"};
+
+    private Button btPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +24,18 @@ public class Principal extends AppCompatActivity implements AdapterView.OnItemCl
         setContentView(R.layout.activity_principal);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemMenu);
 
-        ListView listView = (ListView) findViewById(R.id.listaMenu);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-    }
 
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        btPrincipal = findViewById(R.id.btPrincipal);
 
-        Intent intent = null;
+        btPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        try {
+                Intent intent = new Intent(Principal.this, ListaEvento.class);
 
-            Class obj = Class.forName ("br.edu.iff.pooa20181.ondeir.activity."+activities[position]);
-            intent = new Intent(this, obj);
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        startActivity(intent);
-
+                startActivity(intent);
+            }
+        });
     }
 
 }
